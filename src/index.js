949b41f81d61940b28e2130c43881b7d3f9b08c0
deepak1jha <ddjha171@gmail.com>
2021-01-18 16:46:39 +0530

    Relationship btw post and authors created

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
 import { GraphQLServer } from 'graphql-yoga';
 
-const authorsData =[{
-    id:"1",
-    name :"Author 1",
-    org:"Org 1"
-},{
-    id:"2",
-    name :"Author 2",
-    org:"Org 2"
-},{
-    id:"3",
-    name :"Author 3",
-    org:"Org 3"
+const authorsData = [{
+    id: "1",
+    name: "Author 1",
+    org: "Org 1"
+}, {
+    id: "2",
+    name: "Author 2",
+    org: "Org 2"
+}, {
+    id: "3",
+    name: "Author 3",
+    org: "Org 3"
 }]
 
+const postData = [{
+    id: "1",
+    title: "title 1",
+    body: "Post Body 1",
+    published: true,
+    author: "1"
+}, {
+    id: "2",
+    title: "title 2",
+    body: "Post Body 2",
+    published: true,
+    author: "2"
+}, {
+    id: "3",
+    title: "title 3",
+    body: "Post Body 3",
+    published: false,
+    author: "1"
+}]
 const typeDefs = `
   type Query {
     author : Author!
-    post : Post!
+    post : [Post!]!
     authors :[Author!]!
   }
   
   type Author {
-    id :ID!
+    id : ID!
     name : String!
     org : String!
   }
@@ -31,28 +50,30 @@ const typeDefs = `
     title : String!
     body : String!
     published: Boolean!
+    author : Author!
   }
 `
 
 const resolvers = {
     Query: {
-        author(){
+        author() {
             return {
                 id: "6146868",
-                name :"Author Name",
-                org:"Bonami"
+                name: "Author Name",
+                org: "Bonami"
             }
         },
-        post(){
-            return {
-                id:"5261",
-                title :"Title  1",
-                body :"Body 1",
-                published:true
-            }
+        post(parent, args, ctx, info) {
+            return postData;
         },
-        authors(parent, args, ctx, info){
+        authors(parent, args, ctx, info) {
             return authorsData;
+        }
+    }, Post: {
+        author(parent, args, ctx, info) {
+            return authorsData.find((user) => {
+                return user.id === parent.author;
+            })
         }
     }
 }
